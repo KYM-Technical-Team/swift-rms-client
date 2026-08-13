fdba7e3c4bc4004d184cf05a909c860e4f7db7d8
Emmie <emmiebangs@gmail.com>
2026-08-13 11:31:19 +0000

    chore: update API base URL to point to new render instance

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance, AxiosError, InternalAxiosRequestConfig } from 'ax
 import { ApiResponse, ApiError } from '@/types';
 
 // const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/v1' || "https://swfit-rms-server.onrender.com/api/v1";
-const BASE_URL = "https://swfit-rms-server-xc56.onrender.com/api/v1";
+const BASE_URL = "https://swfit-rms-server-6gii.onrender.com/api/v1";
+
 
 // Create axios instance
 const apiClient: AxiosInstance = axios.create({
